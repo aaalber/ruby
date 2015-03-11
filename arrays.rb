72ea079c461f060a3d26431e7595ff1483f595e3
Aaron Albertina <aaalber@gmail.com>
2015-03-11 14:41:51 +0000

    Create/Delete arrays.rb

---
+++ arrays.rb
@@ -0,0 +1,15 @@
+types = ['crispy', 'raw', 'crunchy', 'grilled']
+
+print types.length
+print types.size
+
+types.push 'smoked'
+
+types << 'deep fried' 
+
+print types.first
+print types.last
+print types[0..1]
+
+
+puts types
