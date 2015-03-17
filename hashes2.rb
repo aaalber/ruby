45e12555f5ea77dd4bf7fece4473ebeae34463aa
Aaron Albertina <aaalber@gmail.com>
2015-03-17 19:24:36 +0000

    Added Hashes example

---
+++ hashes2.rb
@@ -0,0 +1,16 @@
+players = {
+	'McCutchen, Andrew' => {
+		'AVG' => 0.311,
+		'OBP' => 0.385,
+		'SLG' => 0.507
+	},
+	'Alvarez, Pedro' => {
+		'AVG' => 0.236,
+		'OBP' => 0.297,
+		'SLG' => 0.477
+	}
+}
+
+puts players['McCutchen, Andrew']
+
+print players['McCutchen, Andrew']['AVG']
